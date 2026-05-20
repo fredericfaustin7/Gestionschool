6c139397f076ea0932182d559e589ae50835ecc8
fredericfaustin7 <fredericfaustin7@gmail.com>
2026-05-21 00:49:46 +0200

    fix basename router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+     <Router basename="/Gestionschool">
       <Routes>
         <Route path="/" element={<HeaderStudent />} />
         <Route path="/etudiant" element={<HeaderEleve />} />
